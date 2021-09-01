1dca5fb28b795f7e3a22bffe7c892745e2aca9e9
Muhammad Irfani <muhammadirfani340@gmail.com>
2021-09-01 08:49:45 +0700

    first commit

---
+++ 1_machineLearning/main.py
@@ -0,0 +1,53 @@
+msg = "nama saya Muhammad Irfani"
+print("Hello,"+msg)
+
+pangkat = 8 ** 2
+nilai = pangkat
+print(pangkat)
+
+if nilai == 64:
+    print("True")
+else:
+    print("Soory its false")
+
+#penjumlahan
+print("Penjumlahan")
+apel = 1
+jeruk = 2
+print(apel+jeruk)
+
+#pengurangan
+print("pengurangan")
+saldoAwal = 100
+hutang = 50
+print("Saldo anda = ", saldoAwal - hutang)
+
+#perkalian
+print("perkalian")
+nilaiAwal = 5
+nilaiMatkul = 5
+print("Nilai akhir = ", nilaiAwal * nilaiMatkul)
+
+#pembagian
+print("Pembagian")
+jumlahMakanan = 100
+jumlahAnak = 4
+print(jumlahMakanan / jumlahAnak)
+
+print("if condition")
+#if condition
+print()
+answer = str(input('Hello'))
+procced = "hello" or "yes"
+
+if answer == procced:
+    print("Hello There")
+else:
+    print("?")
+
+angka = 8
+if angka > 2:
+    print("if, nilai angka sekarang " + str(angka))
+while angka > 2:
+    print("while, nlai angka sekarang:" + str(angka))
+    angka = angka -1
